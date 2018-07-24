64a1cf2c67752b6ff408b7640d33e723a33f8176
taki maji <majitaki1234@gmail.com>
2018-07-24 13:31:19 +0900

    ba

--- OSM2018/GUIs/NetworkGUI.cs
+++ OSM2018/GUIs/NetworkGUI.cs
@@ -10,26 +10,22 @@ using System.Windows.Forms;
 using OSM2018.Interfaces;
 using OSM2018.Networks;
 
-namespace OSM2018.GUIs
-{
-    public partial class NetworkGUI : UserControl
-    {
-        public NetworkGUI()
-        {
-            InitializeComponent();
-            this.UserInitialize();
-        }
-
-        void UserInitialize()
-        {
-            this.comboBoxWS.SelectedIndex = 0;
-            this.comboBoxSF.SelectedIndex = 0;
-            this.comboBoxRandom.SelectedIndex = 0;
-        }
-
-        private void buttonGenerateGraph_Click(object sender, EventArgs e)
-        {
-            I_Network network = new WS_NetworkGenerator(100, 6, 0.01).Generate(1);
-        }
+namespace OSM2018.GUIs {
+  public partial class NetworkGUI : UserControl {
+    public NetworkGUI() {
+      InitializeComponent();
+      this.UserInitialize();
     }
+
+    void UserInitialize() {
+      this.comboBoxWS.SelectedIndex = 0;
+      this.comboBoxSF.SelectedIndex = 0;
+      this.comboBoxRandom.SelectedIndex = 0;
+    }
+
+    private void buttonGenerateGraph_Click(object sender, EventArgs e) {
+      //I_Network network = new WS_NetworkGenerator(100, 6, 0.01).Generate(1);
+      I_Network network = new BA_NetworkGenerator(100, 6).Generate(1);
+    }
+  }
 }
